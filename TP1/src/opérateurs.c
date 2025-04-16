6d9b5bc15e654ac4f6632fa0dc4484e24123b47f
Aurélien Balavoine <132837748+Sh3z4m@users.noreply.github.com>
2025-04-16 15:08:29 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -6,7 +6,7 @@ int main() {
     int B = 3;
 
     // Affichage des valeurs des variables
-    printf("Valeur de 'char c' : %c\n", A);
+    printf("Valeur de 'char c' : %d\n", A);
     printf("Valeur de 'signed char sc' : %d\n", B);
 
     return 0;
