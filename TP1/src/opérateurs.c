8ca53a9f5e33d0a10a1c0ba1bfcd3ee30ba2a16d
Aurélien Balavoine <132837748+Sh3z4m@users.noreply.github.com>
2025-04-16 15:12:54 +0200

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -5,9 +5,19 @@ int main() {
     int A = 16;
     int B = 3;
 
+    int addition = A + B;
+    int soustraction = B - A;
+    int multiplication = A * B;
+    double division = A / B;
+
     // Affichage des valeurs des variables
-    printf("Valeur de 'char c' : %d\n", A);
-    printf("Valeur de 'signed char sc' : %d\n", B);
+    printf("Valeur de A : %d\n", A);
+    printf("Valeur de B : %d\n", B);
+
+    printf("Addition : %d\n", addition);
+    printf("Soustraction : %d\n", soustraction);
+    printf("Multiplication : %d\n", multiplication);
+    printf("Division : %d\n", division);
 
     return 0;
 }
