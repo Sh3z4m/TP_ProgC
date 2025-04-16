608896bed270295741e0065e7f7fa924006707f0
Aurélien Balavoine <132837748+Sh3z4m@users.noreply.github.com>
2025-04-16 14:25:34 +0200

    Update cercle.c

---
+++ TP1/src/cercle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float rayon;         //rayon 
+    const float PI = 3.14159; // valeur de PI approximative
+    float aire;           // stockagge de l'aire
+    float circonference;  // stockage de la circonférence
+
+
+  printf("Rayon du cercle: ");
+
+    // demande de rayon
+    scanf("%f", &rayon);
+
+    // Calcul de l'aire
+    aire = PI * rayon * rayon;
+
+    // Calcul de la circonference
+    circonference = 2 * PI * rayon;
+
+    printf("\nRayon:\n", rayon);
+    printf("Aire : \n", aire);
+    printf("Circonférence \n", circonference);
+
+    return 0;
+}
